ca66254fe47a47e787e38f95d29168ffa9fd47a3
System Administrator <root@mings-MacBook-Pro.local>
2015-09-23 17:57:56 +0900

    add static dir

--- toureco/settings.py
+++ toureco/settings.py
@@ -14,6 +14,12 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
