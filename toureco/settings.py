63af03494e982e7d9a968873323691d044f3a4fd
System Administrator <root@mings-MacBook-Pro.local>
2015-09-24 12:37:53 +0900

    change static dir

--- toureco/settings.py
+++ toureco/settings.py
@@ -18,7 +18,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'toureco_app/static'),
 )
 
 # Quick-start development settings - unsuitable for production
