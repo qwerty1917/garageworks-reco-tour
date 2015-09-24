ceb1ad921cfba34f38cae83fb435b69024b1f9bc
System Administrator <root@mings-MacBook-Pro.local>
2015-09-24 16:34:19 +0900

    fixes

--- toureco/settings.py
+++ toureco/settings.py
@@ -19,7 +19,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(PROJECT_PATH, '../static'),
 )
 
 # Quick-start development settings - unsuitable for production
--- toureco/urls.py
+++ toureco/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+import settings
 
 urlpatterns = [
     # Examples:
